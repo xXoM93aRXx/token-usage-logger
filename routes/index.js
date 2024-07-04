ae0d97012f51e4521efa9a1fe5819613e12191b2
Omar Mahdy <omar.mahdy246@gmail.com>
2024-07-04 12:01:10 +0800

    Created new collection

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.post('/log-token-usage',async(req,res)=>{
     const {name,inputTokenCount,outputTokenCount,model} = req.body;
 
     try {
-        const existingUsage = await prisma.tokenUsage.findFirst({
+        const existingUsage = await prisma.consumption.findFirst({
             where:{
                 name,
                 model
@@ -16,7 +16,7 @@ router.post('/log-token-usage',async(req,res)=>{
         })
 
         if (existingUsage){
-            const updatedUsage = await prisma.tokenUsage.update({
+            const updatedUsage = await prisma.consumption.update({
                 where:{
                     id:existingUsage.id,
                 },data:{
@@ -26,7 +26,7 @@ router.post('/log-token-usage',async(req,res)=>{
             })
             res.status(200).json(updatedUsage)
         }else{
-            const newTokenUsage = await prisma.tokenUsage.create({
+            const newTokenUsage = await prisma.consumption.create({
                 data:{
                     name,
                     inputTokenCount,
@@ -48,8 +48,8 @@ router.get('/user-token-usage',async(req,res)=>{
 
     try {
         const tokenUsages = name
-        ? await prisma.tokenUsage.findMany({where:{name}})
-        :await prisma.tokenUsage.findMany()
+        ? await prisma.consumption.findMany({where:{name}})
+        :await prisma.consumption.findMany()
     res.status(200).json(tokenUsages)
     } catch (error) {
         console.log(error)
@@ -63,7 +63,7 @@ router.post('/reset-token-usage',async(req,res)=>{
     const {name} = req.body
 
     try {
-       await prisma.tokenUsage.deleteMany({where:{name}}) 
+       await prisma.consumption.deleteMany({where:{name}}) 
        res.send(200).json({message: 'Token usage reset successfully'})
     } catch (error) {
         console.log(error)
